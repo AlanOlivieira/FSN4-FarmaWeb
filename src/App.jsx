93c4af210cc8277df7f06bac72f13e66771a65db
Antônio José <antoniojose000silva@gmail.com>
2025-07-12 23:51:46 -0300

    Atualizações nos arquivos de configuração e novos arquivos de imagem

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route, Outlet } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
+import Sobre from './pages/Sobre'; // Adicionei essa importação
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Layout() {
   return (
@@ -20,6 +23,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="sobre" element={<Sobre />} /> {/* Atualizei esta linha */}
         <Route path="sobre" element={<h1 style={{ padding: '2rem' }}>Sobre</h1>} />
         <Route path="contato" element={<h1 style={{ padding: '2rem' }}>Contato</h1>} />
       </Route>
