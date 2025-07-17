d5c6267dea22088e23d571d21b74f43fe40f1d41
Alan Oliveira <alantvd066@gmail.com>
2025-07-17 10:37:20 -0300

    modificando botao de compra, add carrossel e pagina de detalhe do produto

--- src/components/ProdutoCard.jsx
+++ src/components/ProdutoCard.jsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import { useCarrinho } from "../contexts/CarrinhoContext";
+import BotaoCarrinho from "./BotaoCarrinho";
+import { Link } from "react-router-dom";
 
 export default function ProdutoCard({ produto }) {
-  const { adicionarProduto } = useCarrinho(); //aqui eh pro carrinho oook?
+  const { adicionarProduto } = useCarrinho();
 
   return (
-    // <div className="col-sm-6 col-md-4 mb-4">
-      <div className="card h-100 w-100 shadow-sm">
-        <img
-          src={produto.imagem}
-          alt={produto.nome}
-          className="card-img-top"
-          style={{ height: '200px', objectFit: 'contain' }}
-        />
-        <div className="card-body text-left">
-          <h5 className="card-title">{produto.nome}</h5>
-          <p className="card-text">{produto.descricao}</p>
-          <div className="d-flex flex-row align-items-center justify-content-between"> 
-            <p className="card-text fw-bold">R$ {produto.preco.toFixed(2)}</p>
-            <button
-              className="btn btn-primary btn-sm h-25 w-25 rounded-circle d-flex align-items-center justify-content-center"
-              onClick={() => adicionarProduto(produto)}
-            >
-              <i class="bi bi-bag-plus fs-4"></i>
-            </button>
-          </div>
-        </div>
+    <div className="card h-100 w-100 shadow-sm position-relative">
+      <img
+        src={produto.imagem}
+        alt={produto.nome}
+        className="card-img-top"
+        style={{ height: '200px', objectFit: 'contain' }}
+      />
+
+      <div
+        className="position-absolute"
+        style={{ top: '10px', right: '10px', zIndex: 1 }}
+      >
+        <BotaoCarrinho onClick={() => adicionarProduto(produto)} />
       </div>
-    // </div>
+
+      <div className="card-body d-flex flex-column">
+        <h5 className="card-title">{produto.nome}</h5>
+        <p className="card-text" style={{ flexGrow: 1 }}>
+          {produto.descricao}
+        </p>
+        <p className="card-text fw-bold">R$ {produto.preco.toFixed(2)}</p>
+      </div>
+    </div>
   );
 }
