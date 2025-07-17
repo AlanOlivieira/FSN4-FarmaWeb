d5b090413bf018595d9b12e922588b66e67b5a70
gildeanM <gildean.m098@gmail.com>
2025-07-17 11:21:41 -0300

    feat: add paginação nas categorias de produtos

--- src/pages/nutricao.jsx
+++ src/pages/nutricao.jsx
@@ -1,17 +1,43 @@
+import "./pagination.css"
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import produtos from '../data/produtos';
 import ProdutoCard from '../components/ProdutoCard';
 
+
 export default function Nutricao() {
+const [currentPage, setCurrentPage] = useState(0)
+  const data = produtos.nutricao
+  const itemsPerPage = 10
+
+  const handlePageClick = (data) => {
+    setCurrentPage(data.selected)
+  }
+
+  const offset = currentPage * itemsPerPage
+  const currentData = data.slice(offset, offset + itemsPerPage)
+
+  const pageCount = Math.ceil(data.length / itemsPerPage)
+
+
   return (
     <main className="container mt-4">
       <h2>Dieta & Nutrição</h2>
       <div className="row">
-        {produtos.nutricao.map((produto) => (
+        {currentData.map((produto) => (
           <div className="col-6 col-md-4 col-lg-3  mb-4" key={produto.id}>
             <ProdutoCard produto={produto} />
           </div>
         ))}
       </div>
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        activeClassName={"active"}
+      />
     </main>
   );
 }
